d20de34d94fbc08dfd2e998103a603465514c777
mkerror98 <121492344+mkerror98@users.noreply.github.com>
2023-01-16 11:44:16 +0900

    [Bronze V] Title: A+B, Time: 216 ms, Memory: 17688 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int num1, num2;
+        
+        num1 = sc.nextInt();
+        num2 = sc.nextInt();
+        
+        System.out.println(num1+num2);
+    }
+}
