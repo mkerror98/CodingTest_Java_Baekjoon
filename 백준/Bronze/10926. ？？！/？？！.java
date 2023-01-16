1cd8bea8b94c5854568f1ec347051dda067c9827
mkerror98 <121492344+mkerror98@users.noreply.github.com>
2023-01-16 15:02:30 +0900

    [Bronze V] Title: ??!, Time: 212 ms, Memory: 17756 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Main {
+	public static void main(String[] args) {
+		String name;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		name = sc.nextLine();
+		
+		System.out.println(name + "??!");
+		
+	}
+}
+
