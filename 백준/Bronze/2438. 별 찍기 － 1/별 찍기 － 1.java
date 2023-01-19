91223e84c5bf73207b54c56860789c2c82a0dca9
mkerror98 <121492344+mkerror98@users.noreply.github.com>
2023-01-19 11:15:25 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 304 ms, Memory: 18088 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,19 @@
+// 첫째 줄에는 별 1개, 둘째 줄에는 별 2개, N번째 줄에는 별 N개를 찍는 문제
+
+import java.util.*;
+
+class Main {
+	public static void main(String[] args) {
+		int Line;
+		Scanner sc = new Scanner(System.in);
+		
+		Line = sc.nextInt();
+		
+		for(int i=1; i<=Line; i++) {
+			for(int j=1; j<=i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
