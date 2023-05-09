9a604511daa9346daee05ba1587de5b6285a3df7
mkerror98 <121492344+mkerror98@users.noreply.github.com>
2023-05-09 20:18:02 +0900

    [Bronze V] Title: 문자와 문자열, Time: 212 ms, Memory: 17728 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String str1 = sc.nextLine();
+        int index = sc.nextInt();
+
+        System.out.println(str1.charAt(index-1));
+
+
+    }
+}
